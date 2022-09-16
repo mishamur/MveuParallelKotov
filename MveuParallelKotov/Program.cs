9ce7749575efc077f7eb58e2436777fefc11165b
Михаил Котов <misha.kotov.01@list.ru>
2022-09-16 22:06:36 +0400

    try2

--- MveuParallelKotov/Program.cs
+++ MveuParallelKotov/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using ArchiveFiles;
+using Log;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("запустился архиватор...");
+        Archiver archiver = new Archiver(new ConsoleLogger());
+        archiver.ArchiveFolder(@"C:\Users\User\Documents\mveuC#");
+
+        Console.WriteLine("хотите закончить архивирование? нажмите esc");
+
+        if (Console.ReadKey().Key == ConsoleKey.Escape)
+        {
+            archiver.CancelArchive();
+        }
+    }
+
+}
